4823fdb6ff1ecfbdbe7317b75360ae1c5907bf93
zuoyou.xb <ares@come-future.com>
2023-11-09 20:13:33 +0800

    feat: 增加版本设计/add version design

---
+++ version/src/main/java/cn/ares/boot/version/AresBootVersion.java
@@ -0,0 +1,25 @@
+package cn.ares.boot.version;
+
+/**
+ * @author: Ares
+ * @time: 2023-11-09 17:41:18
+ * @description: Ares boot version
+ * @version: JDK 1.8
+ */
+public class AresBootVersion {
+
+  /**
+   * 版本键 Version key
+   */
+  public static final String VERSION_KEY = "ares.boot.version";
+
+  /**
+   * 版本值 Version value
+   */
+  private static final String VERSION_VALUE = "1.0.1-SNAPSHOT";
+
+  public static String getVersion() {
+    return VERSION_VALUE;
+  }
+
+}

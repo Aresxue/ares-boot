a0d2681fbfd963994b4574c610a3b78e7633f391
zuoyou.xb <ares@come-future.com>
2023-11-16 10:40:43 +0800

    feat: 支持提取classes直接输出为gzip文件/Support to extract classes directly output to gzip file

--- util/ares-boot-util-spring/src/main/java/cn/ares/boot/util/spring/SpringBootClassesExtractor.java
+++ util/ares-boot-util-spring/src/main/java/cn/ares/boot/util/spring/SpringBootClassesExtractor.java
@@ -1,13 +1,19 @@
 package cn.ares.boot.util.spring;
 
+import cn.ares.boot.util.common.StringUtil;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.jar.JarEntry;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.jar.JarFile;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.FileCopyUtils;
 
 /**
  * @author: Ares
@@ -28,38 +34,63 @@ public class SpringBootClassesExtractor {
 
   /**
    * @author: Ares
-   * @description: 提取spring boot fat jar下BOOT-INF/classes/下的class文件
+   * @description: 提取spring boot fat jar下BOOT-INF/classes/下的class文件输出到文件夹
+   * @description: Extract the class file under boot-INF /classes/ in spring boot fat jar and output
+   * it to the folder
    * @time: 2023-11-15 10:52:34
-   * @params: [jarPath, classesPath] fat jar路径，输出路径
+   * @params: [jarPath, classesDir] fat jar路径，输出文件夹路径
    * @return: void
    */
-  public static void extractClasses(String jarPath, String classesPath) throws IOException {
+  public static void extractClasses(String jarPath, String classesDir) throws IOException {
     try (JarFile jarFile = new JarFile(jarPath)) {
       jarFile.stream().filter(entry -> entry.getName().startsWith(REPACKAGED_CLASSES_LOCATION))
           .forEach(entry -> {
             try {
-              extractFile(jarFile, entry, classesPath);
+              String relativePath = entry.getName().replace(REPACKAGED_CLASSES_LOCATION, "");
+              File outputFile = new File(classesDir, relativePath);
+              if (entry.isDirectory()) {
+                outputFile.mkdirs();
+              } else {
+                try (InputStream inputStream = jarFile.getInputStream(
+                    entry); FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+                  FileCopyUtils.copy(inputStream, fileOutputStream);
+                }
+              }
             } catch (Exception e) {
-              LOGGER.warn("extract class: {} exception: ", entry.getName(), e);
+              LOGGER.warn("extract file: {} exception: ", entry.getName(), e);
             }
           });
     }
   }
 
-  private static void extractFile(JarFile jarFile, JarEntry entry, String outputDir)
-      throws IOException {
-    File outputFile = new File(outputDir,
-        entry.getName().replace(REPACKAGED_CLASSES_LOCATION, ""));
-    if (entry.isDirectory()) {
-      outputFile.mkdirs();
-    } else {
-      try (InputStream inputStream = jarFile.getInputStream(
-          entry); FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-          fileOutputStream.write(buffer, 0, bytesRead);
-        }
+  /**
+   * @author: Ares
+   * @description: 提取spring boot fat jar下BOOT-INF/classes/下的class文件并输出成gzip文件
+   * @description: Extract the class file under boot-INF /classes/ in spring boot fat jar and output
+   * it as a gzip file
+   * @time: 2023-11-16 10:11:34
+   * @params: [jarPath, classesGzPath] fat jar路径，输出gzip文件路径
+   * @return: void
+   */
+  public static void extractClassesGzip(String jarPath, String classesGzPath) throws IOException {
+    try (ZipOutputStream zipOutputStream = new ZipOutputStream(new GZIPOutputStream(
+        new GZIPOutputStream(Files.newOutputStream(Paths.get(classesGzPath)))))) {
+      try (JarFile jarFile = new JarFile(jarPath)) {
+        jarFile.stream().filter(entry -> entry.getName().startsWith(REPACKAGED_CLASSES_LOCATION))
+            .forEach(entry -> {
+              try {
+                try (InputStream inputStream = jarFile.getInputStream(entry)) {
+                  String relativePath = entry.getName().replace(REPACKAGED_CLASSES_LOCATION, "");
+                  if (StringUtil.isNotBlank(relativePath)) {
+                    zipOutputStream.putNextEntry(new ZipEntry(relativePath));
+                    FileCopyUtils.copy(inputStream, zipOutputStream);
+                    zipOutputStream.closeEntry();
+                  }
+                }
+              } catch (Exception e) {
+                LOGGER.warn("extract file: {} exception: ", entry.getName(), e);
+              }
+            });
       }
     }
   }

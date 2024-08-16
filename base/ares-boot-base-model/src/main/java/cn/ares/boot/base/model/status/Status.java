09aca4afef05a9cdf06fc1e658441abd62d2aaf4
xuebing <xuebing@xiaohongshu.com>
2024-08-16 14:49:11 +0800

    feat: provider parse code method

--- base/ares-boot-base-model/src/main/java/cn/ares/boot/base/model/status/Status.java
+++ base/ares-boot-base-model/src/main/java/cn/ares/boot/base/model/status/Status.java
@@ -29,4 +29,54 @@ public interface Status {
    */
   String getMessage();
 
+  /**
+   * @author: Ares
+   * @description: 获取应用码
+   * @time: 2024-08-16 14:49:23
+   * @return: int 应用码
+   */
+  default int getAppCode() {
+    return extractAppCode(getCode());
+  }
+
+  /**
+   * @author: Ares
+   * @description: 从错误码中提取应用码
+   * @time: 2024-08-16 14:49:46
+   * @params: [code] 错误码
+   * @return: int 应用码
+   */
+  static int extractAppCode(int code) {
+    String codeStr = String.valueOf(code);
+    if (codeStr.length() != 9) {
+      throw new IllegalStateException("status code must be 9 digits");
+    }
+    return Integer.parseInt(codeStr.substring(0, 3));
+  }
+
+  /**
+   * @author: Ares
+   * @description: 获取详情码
+   * @time: 2024-08-16 14:50:09
+   * @return: int 详情码
+   */
+  default int getDetailCode() {
+    return extractDetailCode(getCode());
+  }
+
+  /**
+   * @author: Ares
+   * @description: 从错误码中提取详情码
+   * @time: 2024-08-16 14:50:57
+   * @params: [code] 错误码
+   * @return: int 详情码
+   */
+  static int extractDetailCode(int code) {
+    String codeStr = String.valueOf(code);
+    if (codeStr.length() != 9) {
+      throw new IllegalStateException("status code must be 9 digits");
+    }
+    return Integer.parseInt(codeStr.substring(7));
+  }
+
 }

10e48c3505609f6c2afe9ceaf89589a02c91ea11
xuebing <xuebing@xiaohongshu.com>
2024-10-17 17:23:49 +0800

    feat: 增强OrmUtil的模糊查询功能/Enhance the fuzzy query function of OrmUtil

--- starter/ares-boot-starter-datasource-related/ares-boot-starter-datasource/src/main/java/cn/ares/boot/starter/datasource/util/OrmUtil.java
+++ starter/ares-boot-starter-datasource-related/ares-boot-starter-datasource/src/main/java/cn/ares/boot/starter/datasource/util/OrmUtil.java
@@ -1,10 +1,10 @@
 package cn.ares.boot.starter.datasource.util;
 
-import static cn.ares.boot.util.common.constant.SymbolConstant.ASTERISK;
+import static cn.ares.boot.util.common.constant.SymbolConstant.PER_CENT;
+import static com.baomidou.mybatisplus.core.toolkit.StringPool.ASTERISK;
 
 import cn.ares.boot.util.common.StringUtil;
 import com.baomidou.mybatisplus.core.conditions.AbstractWrapper;
-import java.util.Optional;
 
 /**
  * @author: Ares
@@ -22,20 +22,37 @@ public class OrmUtil {
    */
   public static <T, R, Children extends AbstractWrapper<T, R, Children>> void eqOrLike(
       AbstractWrapper<T, R, Children> abstractWrapper, boolean condition, R column, String value) {
-    value = Optional.ofNullable(value).map(String::trim).orElse(null);
     if (StringUtil.isNotBlank(value)) {
-      if (value.startsWith(ASTERISK) && value.endsWith(ASTERISK)) {
-        abstractWrapper.like(condition, column, StringUtil.trim(value, ASTERISK));
-      } else if (value.startsWith(ASTERISK)) {
-        abstractWrapper.likeLeft(condition, column, StringUtil.trim(value, ASTERISK));
-      } else if (value.endsWith(ASTERISK)) {
-        abstractWrapper.likeRight(condition, column, StringUtil.trim(value, ASTERISK));
+      if (value.contains(ASTERISK)) {
+        // 将*转为%
+        String sqlValue = StringUtil.replace(value, ASTERISK, PER_CENT);
+        if (value.startsWith(ASTERISK)) {
+          if (value.endsWith(ASTERISK)) {
+            // 会在左右都拼上%，在解析和执行like查询时，一般会优化掉连续的%，所以多个%不会影响实际的查询性能
+            // will be spliced on both sides with %, in parsing and executing like queries, continuous % will generally be optimized, so multiple % will not affect the actual query performance
+            // *abc* -> %abc% -> %%abc%%
+            abstractWrapper.like(condition, column, sqlValue);
+          } else {
+            // 会在左拼上%，在解析和执行like查询时，一般会优化掉连续的%，所以多个%不会影响实际的查询性能
+            // will be spliced on the left with %, in parsing and executing like queries, continuous % will generally be optimized, so multiple % will not affect the actual query performance
+            // *abc -> %abc -> %%abc
+            abstractWrapper.likeLeft(condition, column, sqlValue);
+          }
+        } else if (value.endsWith(ASTERISK)) {
+          // 会在右拼上%，在解析和执行like查询时，一般会优化掉连续的%，所以多个%不会影响实际的查询性能
+          // will be spliced on the right with %, in parsing and executing like queries, continuous % will generally be optimized, so multiple % will not affect the actual query performance
+          // abc* -> abc% -> abc%%
+          abstractWrapper.likeRight(condition, column, sqlValue);
+        }
+        // 会在右拼上%，可能会影响实际的查询性能
+        // will be spliced on the right with %, which may affect the actual query performance
+        // ab*c -> ab%c -> ab%c%
+        abstractWrapper.likeRight(condition, column, sqlValue);
       } else {
         abstractWrapper.eq(condition, column, value);
       }
     }
   }
-
 
   /**
    * @author: Ares

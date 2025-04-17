568fab3d4e79380ac21f4f06f8a0da46d8cecb33
xuebing <xuebing@xiaohongshu.com>
2025-04-17 11:42:09 +0800

    fix: 修复cn.ares.boot.base.log.util.RePutRejectedExecutionHandler潜在的死锁问题

--- base/ares-boot-base-log/src/main/java/cn/ares/boot/base/log/util/RePutRejectedExecutionHandler.java
+++ base/ares-boot-base-log/src/main/java/cn/ares/boot/base/log/util/RePutRejectedExecutionHandler.java
@@ -1,20 +1,38 @@
 package cn.ares.boot.base.log.util;
 
+import java.util.Set;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author: Ares
  * @time: 2022-10-25 14:03:55
+ * @description: 线程池队列满载时重新放入任务
  * @description: Re-put the task when thread pool task queue is fully
- * @description: 线程池满载时重新放入任务
  * @version: JDK 1.8
  */
 public class RePutRejectedExecutionHandler implements RejectedExecutionHandler {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(RePutRejectedExecutionHandler.class);
+
+  /**
+   * 使用当前拒绝策略的线程的工作线程集合 The set of worker threads that use the current rejection policy
+   */
+  private final Set<Thread> workerThreadSet;
+  /**
+   * 备选的拒绝策略 Backup rejection policy
+   */
+  private final RejectedExecutionHandler backupRejectedExecutionHandler;
+
+  public RePutRejectedExecutionHandler(Set<Thread> workerThreadSet, RejectedExecutionHandler handler) {
+    this.workerThreadSet = workerThreadSet;
+    // 默认使用拒绝策略和jdk
+    // 默认使用拒绝策略
+    this.backupRejectedExecutionHandler = null == handler ? new AbortPolicy() : handler;
+  }
 
   @Override
   public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
@@ -22,28 +40,23 @@ public class RePutRejectedExecutionHandler implements RejectedExecutionHandler {
       LOGGER.warn("thread pool is shutdown");
       return;
     }
+    // 检查当前线程是否属于线程池的工作线程
+    // Check whether the current thread belongs to the worker thread of the thread pool
+    if (workerThreadSet.contains(Thread.currentThread())) {
+      // 直接使用备份的拒绝策略
+      // Directly use the backup rejection policy
+      backupRejectedExecutionHandler.rejectedExecution(runnable, executor);
+      return;
+    }
     LOGGER.warn("process that the thread pool is fully loaded and re-put tasks");
-    // put is blocking
     // put是阻塞的
+    // put is blocking
     try {
       executor.getQueue().put(runnable);
     } catch (InterruptedException interruptedException) {
       LOGGER.error("re-put the task exception:", interruptedException);
       Thread.currentThread().interrupt();
     }
-  }
-
-  private static class LazyHolder {
-
-    private static final RePutRejectedExecutionHandler INSTANCE = new RePutRejectedExecutionHandler();
-  }
-
-  private RePutRejectedExecutionHandler() {
-
-  }
-
-  public static RePutRejectedExecutionHandler getInstance() {
-    return RePutRejectedExecutionHandler.LazyHolder.INSTANCE;
   }
 
 }
---
+++ base/ares-boot-base-log/src/main/java/cn/ares/boot/base/log/util/RePutThreadPoolExecutor.java
@@ -0,0 +1,98 @@
+package cn.ares.boot.base.log.util;
+
+import cn.ares.boot.util.common.structure.ConcurrentHashSet;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author: Ares
+ * @time: 2025-04-16 20:44:19
+ * @description: 队列满载时重新放入任务的线程池
+ * @description: Re-put the task when thread pool task queue is fully
+ * @version: JDK 1.8
+ */
+public class RePutThreadPoolExecutor extends ThreadPoolExecutor {
+
+  private final Set<Thread> workerThreadSet;
+
+  public RePutThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+      @NotNull TimeUnit unit, @NotNull BlockingQueue<Runnable> workQueue,
+      @NotNull RejectedExecutionHandler handler) {
+    super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    workerThreadSet = initWorkerThreadSet(handler);
+  }
+
+  public RePutThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+      @NotNull TimeUnit unit, @NotNull BlockingQueue<Runnable> workQueue,
+      @NotNull ThreadFactory threadFactory, @NotNull RejectedExecutionHandler handler) {
+    super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    workerThreadSet = initWorkerThreadSet(handler);
+  }
+
+
+  public RePutThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+      @NotNull TimeUnit unit, @NotNull BlockingQueue<Runnable> workQueue) {
+    super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    workerThreadSet = initWorkerThreadSet(null);
+  }
+
+  public RePutThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+      @NotNull TimeUnit unit, @NotNull BlockingQueue<Runnable> workQueue,
+      @NotNull ThreadFactory threadFactory) {
+    super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    workerThreadSet = initWorkerThreadSet(null);
+  }
+
+  @NotNull
+  private Set<Thread> initWorkerThreadSet(RejectedExecutionHandler handler) {
+    Set<Thread> set = new ConcurrentHashSet<>();
+    RePutRejectedExecutionHandler executionHandler = new RePutRejectedExecutionHandler(set, handler);
+    super.setRejectedExecutionHandler(executionHandler);
+    return set;
+  }
+
+  @Override
+  protected void beforeExecute(Thread thread, Runnable runnable) {
+    // 记录工作线程
+    workerThreadSet.add(thread);
+    super.beforeExecute(thread, runnable);
+  }
+
+  @Override
+  protected void afterExecute(Runnable runnable, Throwable throwable) {
+    try {
+      super.afterExecute(runnable, throwable);
+    } finally {
+      workerThreadSet.remove(Thread.currentThread());
+    }
+  }
+
+  @Override
+  protected void terminated() {
+    super.terminated();
+    workerThreadSet.clear();
+  }
+
+
+  /**
+   * 获取当前所有工作线程的集合
+   */
+  public Set<Thread> getWorkerThreadSet() {
+    return Collections.unmodifiableSet(workerThreadSet);
+  }
+
+  /**
+   * 获取当前工作线程数
+   */
+  public int getWorkerThreadCount() {
+    return workerThreadSet.size();
+  }
+
+
+}

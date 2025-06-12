7f29ed3058845a5cb2c3b4eb7111f0b83da7dd91
xuebing <xuebing@xiaohongshu.com>
2025-06-12 10:59:59 +0800

    refactor: task rename timed-job feat: add cn.ares.boot.util.spring.PathMatcherUtil#extractUriTemplateVariables/extractPathWithinPattern method

--- util/ares-boot-util-spring/src/main/java/cn/ares/boot/util/spring/PathMatcherUtil.java
+++ util/ares-boot-util-spring/src/main/java/cn/ares/boot/util/spring/PathMatcherUtil.java
@@ -4,6 +4,7 @@ import cn.ares.boot.util.common.ArrayUtil;
 import cn.ares.boot.util.common.CollectionUtil;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
@@ -16,6 +17,10 @@ import org.springframework.util.PathMatcher;
  */
 public class PathMatcherUtil {
 
+  /**
+   * AntPathMatcher非完全线程安全，不能对此实例进行修改否则可能会引发线程安全问题
+   * 当前实例的方法都不修改AntPathMatcher的内部状态所以是线程安全的
+   */
   private static final PathMatcher PATH_MATCHER = new AntPathMatcher();
 
   /**
@@ -196,6 +201,43 @@ public class PathMatcherUtil {
     } else {
       return Arrays.stream(patterns).noneMatch(pattern -> match(pattern, path));
     }
+  }
+
+  /**
+   * Given a pattern and a full path, determine the pattern-mapped part.
+   * <p>This method is supposed to find out which part of the path is matched
+   * dynamically through an actual pattern, that is, it strips off a statically
+   * defined leading path from the given full path, returning only the actually
+   * pattern-matched part of the path.
+   * <p>For example: For "myroot/*.html" as pattern and "myroot/myfile.html"
+   * as full path, this method should return "myfile.html". The detailed
+   * determination rules are specified to this PathMatcher's matching strategy.
+   * <p>A simple implementation may return the given full path as-is in case
+   * of an actual pattern, and the empty String in case of the pattern not
+   * containing any dynamic parts (i.e. the {@code pattern} parameter being
+   * a static path that wouldn't qualify as an actual {@link #isPattern pattern}).
+   * A sophisticated implementation will differentiate between the static parts
+   * and the dynamic parts of the given path pattern.
+   * @param pattern the path pattern
+   * @param path the full path to introspect
+   * @return the pattern-mapped part of the given {@code path}
+   * (never {@code null})
+   */
+  public static String extractPathWithinPattern(String pattern, String path) {
+    return PATH_MATCHER.extractPathWithinPattern(pattern, path);
+  }
+
+  /**
+   * Given a pattern and a full path, extract the URI template variables. URI template
+   * variables are expressed through curly brackets ('{' and '}').
+   * <p>For example: For pattern "/hotels/{hotel}" and path "/hotels/1", this method will
+   * return a map containing "hotel" &rarr; "1".
+   * @param pattern the path pattern, possibly containing URI templates
+   * @param path the full path to extract template variables from
+   * @return a map, containing variable names as keys; variables values as values
+   */
+  public static Map<String, String> extractUriTemplateVariables(String pattern, String path) {
+    return PATH_MATCHER.extractUriTemplateVariables(pattern, path);
   }
 
 }

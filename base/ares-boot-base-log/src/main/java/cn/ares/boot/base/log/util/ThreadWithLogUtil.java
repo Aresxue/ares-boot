93fa3bb6ab8f460dc4a3b093cf02676a9892e660
xuebing <xuebing@xiaohongshu.com>
2025-10-10 17:22:22 +0800

    perf

--- base/ares-boot-base-log/src/main/java/cn/ares/boot/base/log/util/ThreadWithLogUtil.java
+++ base/ares-boot-base-log/src/main/java/cn/ares/boot/base/log/util/ThreadWithLogUtil.java
@@ -65,7 +65,7 @@ public class ThreadWithLogUtil {
         executor.shutdownNow();
         // Wait a while for tasks to respond to being cancelled.
         if (!executor.awaitTermination(timeout, timeUnit)) {
-          LoggerUtil.warn(executor + "%s didn't terminate!");
+          LoggerUtil.warn(executor + " didn't terminate!");
         }
       }
     } catch (InterruptedException ie) {
